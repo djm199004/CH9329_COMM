0177a7fd529d605774ea2c04634e5d9d1c1c2d59
JM Dong <djm1990@sina.com>
2024-08-14 22:18:04 +0800

    test
    test.py修改

--- test.py
+++ test.py
@@ -2,15 +2,16 @@ import serial
 from ch9329Comm import keyboard
 from ch9329Comm import mouse
 
-serial.ser = serial.Serial('COM4', 115200)  # 开启串口
+serial.ser = serial.Serial('COM7', 115200)  # 开启串口
 
-# # 键盘输出helloworld
-# dc = keyboard.DataComm()
-# dc.send_data('HHEELLLLOO')  # 按下HELLO
-# dc.release()  # 松开
+# 键盘输出helloworld
+dc = keyboard.DataComm()
+dc.send_data('HHEELLLLOO')  # 按下HELLO
+dc.release()  # 松开
 # dc.send_data('WWOORRLLDD')  # 按下WORLD
 # dc.release()  # 松开
 
+#
 
 # （绝对）鼠标移动到屏幕的左上100*100的位置
 
